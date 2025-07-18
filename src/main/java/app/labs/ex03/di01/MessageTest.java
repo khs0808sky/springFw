5fc48d943491247fbba8520155410920fc6e9baa
khs0808sky <117973849+khs0808sky@users.noreply.github.com>
2025-07-18 16:01:38 +0900

    0718_SpringInit

---
+++ src/main/java/app/labs/ex03/di01/MessageTest.java
@@ -0,0 +1,13 @@
+package app.labs.ex03.di01;
+
+public class MessageTest {
+
+	public static void main(String[] args) {
+		Message msg;
+		//msg = new MessageEn();
+		msg = new MessageKo();
+		
+		msg.getMessage();
+	}
+
+}
